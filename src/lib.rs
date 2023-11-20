f8175e690df45de7fc01ee32f417844043593fe7
JustBobinAround <roberthenryjunkins@gmail.com>
2023-11-20 11:26:58 -0500

    added prelude *

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+pub mod prelude {
+    pub use crate::assistant;
+    pub use crate::system;
+    pub use crate::user;
+    pub use crate::completions::*;
+    pub use crate::embeddings::*;
+}
 pub mod completions;
 pub mod embeddings;
 pub mod key;

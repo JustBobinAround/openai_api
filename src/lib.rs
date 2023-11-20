e73a750483f8b62a27532f492d14a2c4e90ed392
JustBobinAround <roberthenryjunkins@gmail.com>
2023-11-20 11:39:39 -0500

    Fixing prelude again...

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod prelude {
     pub use crate::{assistant, system, user};
-    pub use crate::completions::{CompletionRequest, CompletionResponse};
-    pub use crate::embeddings::{EmbeddingRequest, EmbeddingResponse};
+    pub use crate::completions::{CompletionRequest, CompletionResponse, CompletionMessage};
+    pub use crate::embeddings::{EmbeddingRequest, EmbeddingResponse, EmbeddingData};
 }
 pub mod completions;
 pub mod embeddings;

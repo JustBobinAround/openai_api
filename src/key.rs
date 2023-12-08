c35c0623b77045016a93412a70f0da7c54d52a83
JustBobinAround <roberthenryjunkins@gmail.com>
2023-12-08 14:06:22 -0500

    change api key env var to static mutex

--- src/key.rs
+++ src/key.rs
@@ -1,8 +1,15 @@
-use std::env;
+use std::sync::{Arc, Mutex};
+use lazy_static::lazy_static;
 
+lazy_static!(
+    static ref API_KEY: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
+);
+pub fn set_api_key(new_key: String) {
+    if let Ok(mut api_key) = API_KEY.lock() {
+        *api_key = new_key.clone(); 
+    }
+}
 pub fn api_key() -> String {
-    let api_key = env::var("OPENAI_API_KEY")
-        .expect("Failed to get OPENAI_API_KEY env variable");
-
+    let api_key = API_KEY.lock().expect("Failed to get API_KEY mutex lock");
     format!("Bearer {}", api_key)
 }
